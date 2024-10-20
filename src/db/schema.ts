5a5f4e1c9e983900e27588d6f68c971a3d67fe73
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2024-10-21 01:18:25 +0700

    *

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,12 +1,23 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 import { ObjectId } from "bson";
+import { pgTable, integer, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-  id: varchar()
+  id: varchar("id")
     .primaryKey()
     .$defaultFn(() => new ObjectId().toHexString()),
-  name: varchar({ length: 255 }).notNull(),
-  updated_at: integer(),
+  name: varchar("name", { length: 255 }),
   created_at: integer().$defaultFn(() => Math.floor(Date.now() / 1000)),
+  updated_at: integer(),
   deleted_at: integer(),
+});
+
+export const postsTable = pgTable("posts", {
+  id: varchar("id")
+    .primaryKey()
+    .$defaultFn(() => new ObjectId().toHexString()),
+  name: varchar("name", { length: 255 }),
+  created_at: integer().$defaultFn(() => Math.floor(Date.now() / 1000)),
+  updated_at: integer(),
+  deleted_at: integer(),
+  usersId: integer("users_id").references(() => usersTable.id),
 });
--- src/seed.ts
+++ src/seed.ts
@@ -1,12 +1,16 @@
+import { db } from "./db";
+import { postsTable, usersTable } from "./db/schema";
 import * as usersrepository from "./users/repository";
 
 const two = async () => {
-  for (let i = 0; i < 50; i++) {
-    const name = `${Math.floor(Date.now() / 1000)}`;
+  await db.delete(usersTable);
+  await db.delete(postsTable);
+  /* for (let i = 0; i < 50; i++) {
+    const name = `name-${i}`;
     await usersrepository.create(name);
     // sleep for 1 second
     await new Promise((resolve) => setTimeout(resolve, 1000));
-  }
+  } */
 };
 
 two();
